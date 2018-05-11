0d36f91c6c4a08868471cc0125d62e9871bffbd3
Rafel Amer <rafel.amer@claimcenter.net>
2018-05-11 11:11:21 +0200

    Changed the files sha265.[ch] by sha2.[ch] and added the functions privateEncryptOAEPRSA and publicDecryptOAEPRSA

---
+++ rsatests/test17.c
@@ -0,0 +1,51 @@
+#include <mce/mcersa.h>
+#include <mce/oaep.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	BD m, c, n;
+	int ret;
+	PrivateRSAKey r1;
+	m = c = n = NULL;
+	ret = EXIT_FAILURE;
+	if ((r1 = bdReadPrivateRSAKeyFromFile("test.key")) == NULL)
+		goto final;
+
+	if ((m = spReadBDFromFile("M.txt")) == NULL)
+		goto final;
+
+	printf("Actual message = ");
+	spPrintDecimal(m);
+
+	if ((c = privateEncryptOAEPRSA(r1, m)) == NULL)
+		goto final;
+
+	printf("Encrypted message = ");
+	spPrintDecimal(c);
+
+	if ((n = publicDecryptOAEPRSA(r1->pub, c)) == NULL)
+		goto final;
+
+	printf("Decrypted message = ");
+	spPrintDecimal(n);
+
+	if (spCompareAbsoluteValues(m, n) == 0)
+		printf("Encryption and decryption OK\n");
+	else
+		printf
+		    ("Encryption or decryption error. You are a bad programmer\n");
+
+	ret = EXIT_SUCCESS;
+
+ final:
+	freeBD(m);
+	freeBD(c);
+	freePrivateRSAKey(r1);
+	if (ret == EXIT_FAILURE)
+		printf("Error reading the RSA key, encrypting or decrypting\n");
+	return ret;
+}

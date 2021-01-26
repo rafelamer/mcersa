501d01ba85acae4c41cefc79d0a2670500e0e17a
Rafel Amer <rafel.amer@upc.edu>
2021-01-26 20:57:07 +0100

    Modificat rsatests/test00.py

--- rsatests/test00.py
+++ rsatests/test00.py
@@ -1,17 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- utf-8 -*-
 #
 try:
     from cypari import pari
 except:
-    print """To run this program, you have to install the python 
+    print ("""To run this program, you have to install the python
 library cypari with the command
-~# pip install cypari
-"""
-    
+~# pip3 install cypari
+""")
+
+pari.allocatemem(16*1024*1024)
 f = open("primes.txt","r")
 
-for line in f.xreadlines():
+for line in f:
     line = line.replace("\n",'')
     line.strip()
     try:
@@ -19,9 +20,9 @@ for line in f.xreadlines():
     except:
         pass
     if not p.isprime():
-        print p
-        print "is not prime!!!!!!"
+        print (p)
+        print ("is not prime!!!!!!")
     else:
-    	print "The number is prime"
+    	print ("The number is prime")
 
-f.close()    
+f.close()
